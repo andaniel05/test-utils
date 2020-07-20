100ba6d519220fd28872167deb9526bc13f536cf
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-07-20 09:53:28 -0400

    Temp

--- src/AssertsTrait.php
+++ src/AssertsTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Andaniel05\TestUtils;
 
 use PHPUnit\Framework\AssertionFailedError;
+use Brick\VarExporter\VarExporter;
 
 /**
  * @author Andy Daniel Navarro Taño <andaniel05@gmail.com>
@@ -12,6 +13,17 @@ trait AssertsTrait
 {
     public function assertExpectedArrayDiff(array $array1, array $array2, array $expects): void
     {
-        $this->assertEquals($array1, $array2);
+        $diff = array_udiff($array1, $array2, function ($value1, $value2) {
+            return $value1 !== $value2;
+        });
+
+        if (empty($diff)) {
+            $this->assertTrue(true);
+            return;
+        } else {
+            $message = "\nUnexpected Array Diff:\n".VarExporter::export($diff);
+            fwrite(STDERR, $message);
+            throw new AssertionFailedError;
+        }
     }
 }
